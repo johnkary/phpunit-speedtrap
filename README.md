181c04896736ba2cf0f50a87feeb80c67a7dda9a
John Kary <john@johnkary.net>
2014-02-05 01:02:58 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# phpunit-speedtrap
+
+Reports on slow-running tests in your PHPUnit test suite.
+
+Enable by adding the following to your phpunit.xml:
+
+```xml
+<phpunit>
+...
+    <listeners>
+        <listener class="JohnKary\PHPUnit\Listener\SpeedTrapListener">
+            <array>
+                <integer>500</integer> <!-- Slowness threshold in ms -->
+                <integer>10</integer>  <!-- Number of slow tests to report on -->
+            </array>
+        </listener>
+    </listeners>
+</phpunit>
+```
---
+++ src/JohnKary/PHPUnit/Listener/SpeedTrapListener.php
@@ -0,0 +1,259 @@
+<?php
+
+namespace JohnKary\PHPUnit\Listener;
+
+/**
+ * A PHPUnit TestListener that exposes your slowest running tests by outputting
+ * results directly to the console.
+ */
+class SpeedTrapListener implements \PHPUnit_Framework_TestListener
+{
+    /**
+     * Internal tracking for test suites.
+     *
+     * Increments as more suites are run, then decremented as they finish. All
+     * suites have been run when returns to 0.
+     *
+     * @var integer
+     */
+    protected $suites = 0;
+
+    /**
+     * Time in milliseconds at which a test will be considered "slow" and
+     * be reported by this listener.
+     *
+     * @var int
+     */
+    private $slowThreshold;
+
+    /**
+     * Number of tests to report on for slowness.
+     *
+     * @var int
+     */
+    protected $reportLength;
+
+    /**
+     * Collection of slow tests
+     *
+     * @var array
+     */
+    protected $slow = array();
+
+    public function __construct($slowThreshold = 500, $reportLength = 10)
+    {
+        $this->slowThreshold = $slowThreshold;
+        $this->reportLength = $reportLength;
+    }
+
+    /**
+     * An error occurred.
+     *
+     * @param \PHPUnit_Framework_Test $test
+     * @param \Exception              $e
+     * @param float                   $time
+     */
+    public function addError(\PHPUnit_Framework_Test $test, \Exception $e, $time)
+    {
+    }
+
+    /**
+     * A failure occurred.
+     *
+     * @param \PHPUnit_Framework_Test                 $test
+     * @param \PHPUnit_Framework_AssertionFailedError $e
+     * @param float                                   $time
+     */
+    public function addFailure(\PHPUnit_Framework_Test $test, \PHPUnit_Framework_AssertionFailedError $e, $time)
+    {
+    }
+
+    /**
+     * Incomplete test.
+     *
+     * @param \PHPUnit_Framework_Test $test
+     * @param \Exception              $e
+     * @param float                   $time
+     */
+    public function addIncompleteTest(\PHPUnit_Framework_Test $test, \Exception $e, $time)
+    {
+    }
+
+    /**
+     * Skipped test.
+     *
+     * @param \PHPUnit_Framework_Test $test
+     * @param \Exception              $e
+     * @param float                  $time
+     */
+    public function addSkippedTest(\PHPUnit_Framework_Test $test, \Exception $e, $time)
+    {
+    }
+
+    /**
+     * A test started.
+     *
+     * @param \PHPUnit_Framework_Test $test
+     */
+    public function startTest(\PHPUnit_Framework_Test $test)
+    {
+    }
+
+    /**
+     * A test ended.
+     *
+     * @param \PHPUnit_Framework_Test $test
+     * @param float                  $time
+     */
+    public function endTest(\PHPUnit_Framework_Test $test, $time)
+    {
+        $time = $this->toMilliseconds($time);
+        if ($this->isSlow($time)) {
+            $this->addSlowTest($test, $time);
+        }
+    }
+
+    /**
+     * A test suite started.
+     *
+     * @param \PHPUnit_Framework_TestSuite $suite
+     */
+    public function startTestSuite(\PHPUnit_Framework_TestSuite $suite)
+    {
+        $this->suites++;
+    }
+
+    /**
+     * A test suite ended.
+     *
+     * @param \PHPUnit_Framework_TestSuite $suite
+     */
+    public function endTestSuite(\PHPUnit_Framework_TestSuite $suite)
+    {
+        $this->suites--;
+
+        if (0 === $this->suites && $this->hasSlowTests()) {
+            arsort($this->slow); // Sort longest running tests to the top
+
+            $this->renderHeader();
+            $this->renderBody();
+            $this->renderFooter();
+        }
+    }
+
+    /**
+     * Whether the given test execution time is considered slow.
+     *
+     * @param int $time Test execution time in milliseconds
+     * @return bool
+     */
+    protected function isSlow($time)
+    {
+        return $time >= $this->slowThreshold;
+    }
+
+    /**
+     * Stores a test as slow.
+     *
+     * @param \PHPUnit_Framework_TestCase $test
+     * @param int                         $time Test execution time in milliseconds
+     */
+    protected function addSlowTest(\PHPUnit_Framework_TestCase $test, $time)
+    {
+        $label = $this->makeLabel($test);
+
+        $this->slow[$label] = $time;
+    }
+
+    /**
+     * Whether at least one test has been considered slow.
+     *
+     * @return bool
+     */
+    protected function hasSlowTests()
+    {
+        return !empty($this->slow);
+    }
+
+    /**
+     * Convert PHPUnit's reported test time (microseconds) to milliseconds.
+     *
+     * @param float $time
+     * @return int
+     */
+    private function toMilliseconds($time)
+    {
+        return (int) round($time * 1000);
+    }
+
+    /**
+     * Label for describing a test
+     *
+     * @param \PHPUnit_Framework_TestCase $test
+     * @return string
+     */
+    private function makeLabel(\PHPUnit_Framework_TestCase $test)
+    {
+        return sprintf('%s:%s', get_class($test), $test->getName());
+    }
+
+    /**
+     * Calculate number of slow tests to report about.
+     *
+     * @return int
+     */
+    private function getReportLength()
+    {
+        return min(count($this->slow), $this->reportLength);
+    }
+
+    /**
+     * Find how many slow tests occurred that won't be shown due to list length.
+     *
+     * @return int Number of hidden slow tests
+     */
+    private function getHiddenCount()
+    {
+        $total = count($this->slow);
+        $showing = $this->getReportLength($this->slow);
+
+        $hidden = 0;
+        if ($total > $showing) {
+            $hidden = $total - $showing;
+        }
+
+        return $hidden;
+    }
+
+    /**
+     * Renders slow test report header
+     */
+    protected function renderHeader()
+    {
+        echo sprintf("\n\nYou should really fix these slow tests (>%sms)...\n", $this->slowThreshold);
+    }
+
+    /**
+     * Renders slow test report body
+     */
+    protected function renderBody()
+    {
+        $length = $this->getReportLength($this->slow);
+        for ($i = 1; $i <= $length; ++$i) {
+            $label = key($this->slow);
+            $time = array_shift($this->slow);
+
+            echo sprintf(" %s. %sms to run %s\n", $i, $time, $label);
+        }
+    }
+
+    /**
+     * Renders slow test report footer
+     */
+    protected function renderFooter()
+    {
+        if ($hidden = $this->getHiddenCount($this->slow)) {
+            echo sprintf("...and there are %s more above your threshold hidden from view", $hidden);
+        }
+    }
+}
