cfdc51a1ad02453abdbee241cc5faa3280009f8d
Barney Laurance <barney@redmagic.org.uk>
2021-05-23 16:30:17 +0100

    Add test for reporting on results from two tests

--- tests/SpeedTrapListenerTest.php
+++ tests/SpeedTrapListenerTest.php
@@ -46,6 +46,38 @@ You should really speed up these slow tests (>500ms)...
         );
     }
 
+
+    public function testItDisplaysTotalSlowTestTime(): void
+    {
+        $testSuite = new TestSuite();
+
+        $test1 = new self('test one');
+        $test2 = new self('test two');
+
+        $this->speedTrapListener->startTestSuite($testSuite);
+        $this->speedTrapListener->startTest($test1);
+        $this->speedTrapListener->endTest($test1, 2 /* duration in seconds */);
+
+
+        $this->speedTrapListener->startTest($test2);
+        $this->speedTrapListener->endTest($test2, 1.5);
+
+        $output = $this->captureOutput(function () use ($testSuite) {
+            $this->speedTrapListener->endTestSuite($testSuite);
+        });
+
+        self::assertSame(
+            '
+
+You should really speed up these slow tests (>500ms)...
+ 1. 2000ms to run JohnKary\\\\PHPUnit\\\\Listener\\\\Tests\\\\SpeedTrapListenerTest::test one
+ 2. 1500ms to run JohnKary\\\\PHPUnit\\\\Listener\\\\Tests\\\\SpeedTrapListenerTest::test two
+'
+            ,
+            $output
+        );
+    }
+
     private function captureOutput(\Closure $closure): string
     {
         ob_start();
